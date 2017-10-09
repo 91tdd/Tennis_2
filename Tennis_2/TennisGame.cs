27431b1bed66fc3e966f775c8fcc13208cc4dd12
hatelove <jay.hatelove@gmail.com>
2017-10-09 19:44:48 +0800

    pass test case: Fifteen Love

--- Tennis_2/TennisGame.cs
+++ Tennis_2/TennisGame.cs
@@ -1,17 +1,21 @@
-﻿using System;
-
-namespace Tennis_2
+﻿namespace Tennis_2
 {
     public class TennisGame
     {
+        private int _firstPlayerScoreTimes;
+
         public string GetScore()
         {
+            if (this._firstPlayerScoreTimes == 1)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
         }
 
         public void FirstPlayerScore()
         {
-            throw new NotImplementedException();
+            this._firstPlayerScoreTimes++;
         }
     }
 }
